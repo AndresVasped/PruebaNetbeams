6ff5e2b9e8a8b8f384269698cefe51a602b93cc1
AliVasped200 <152574439+AndresVasped@users.noreply.github.com>
2024-10-26 11:16:36 -0500

    Uso de netbeams

---
+++ src/pruebanetbeams/PruebaNetbeams.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pruebanetbeams;
+
+/**
+ *
+ * @author VASQUEZ
+ */
+public class PruebaNetbeams {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        //sstem.out.println();
+        System.out.println("La suma es "+suma(5,6));
+    }
+    private static int suma(int x,int y)
+    {
+        return x+y;
+    }
+    
+}
